d18d6347060f310ee1d458ace008a8e4974ed4fe
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-12-13 20:31:47 +0900

    add: msw mock setting

---
+++ app/__tests__/handlers/HelloHandler.ts
@@ -0,0 +1,11 @@
+import { http, HttpResponse, type RequestHandler } from "msw";
+import { mockPost } from "./../mockDatas/mockPost";
+
+const helloHandler = http.get(
+	"https://jsonplaceholder.typicode.com/posts/1",
+	() => {
+		return HttpResponse.json(mockPost);
+	}
+);
+
+export const handlers: RequestHandler[] = [helloHandler];
--- app/__tests__/page.test.tsx
+++ app/__tests__/page.test.tsx
@@ -1,6 +1,6 @@
 import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
-import Home from "./page";
+import Home from "../page";
 
 test("<Home>", () => {
 	render(<Home />);
