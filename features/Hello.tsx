14e1e2a499a4f098a68686cf4fd1671d94005fd9
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-12-16 18:52:14 +0900

    add: 簡易クイズ一覧ビュー

--- features/Hello.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { fetchHello } from './fetchHello';
-
-export default async function Hello() {
-    const { data } = await fetchHello();
-    return <div>{data.title}</div>;
-}
