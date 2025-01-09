e9c3b5f446f4ae251ae9a88c3197828e63bbfed3
kaitokosuge <kaitokosuge.mi@gmail.com>
2025-01-09 23:59:00 +0900

    add: use cacheの準備

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     /* config options here */
+    // experimental: {
+    //     dynamicIO: true,
+    // },
+    experimental: {
+        dynamicIO: true,
+    },
 };
 
 export default nextConfig;
