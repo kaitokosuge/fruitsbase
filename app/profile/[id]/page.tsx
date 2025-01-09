36e09a58049d9681f98a0d654f58768efa6483fc
kaitokosuge <kaitokosuge.mi@gmail.com>
2025-01-10 00:34:29 +0900

    add: 不要なユーザー取得削除

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -25,14 +25,11 @@ export default async function page({
 }) {
     const { id } = await params;
     const { userId } = await auth();
-    // const userId = 'user_2r4i4LxZWE3DZZNChEFKPhJ4u9Y';
-
-    const existingUserRecord = await prisma.user.findUnique({
+    const existingUserRecord = await prisma.user.findFirst({
         where: {
             id: id,
         },
     });
-    console.log('存在してる', existingUserRecord);
     if (!existingUserRecord) {
         return (
             <div>
@@ -43,11 +40,6 @@ export default async function page({
             </div>
         );
     }
-    const userData = await prisma.user.findFirst({
-        where: {
-            id: id,
-        },
-    });
     return (
         <div>
             <Header />
@@ -55,15 +47,15 @@ export default async function page({
                 <div className="flex flex-wrap justify-between w-full mt-10">
                     <div className="md:w-[20%] w-full">
                         <div className="flex items-center">
-                            {userData?.image && (
+                            {existingUserRecord?.image && (
                                 <img
-                                    src={userData?.image}
+                                    src={existingUserRecord?.image}
                                     alt="profile"
                                     className="w-[80px] h-[80px] rounded-full"
                                 />
                             )}
                             <h2 className="font-bold text-xl ml-5">
-                                {userData?.username}
+                                {existingUserRecord?.username}
                             </h2>
                         </div>
                         {/* <p className="text-gray-400 border-b border-blue-900 w-fit pb-1 mt-10">
@@ -72,49 +64,57 @@ export default async function page({
                         <div className="mt-3">
                             <CategoryViews />
                         </div> */}
-                        {userId === id && userId !== null && userData && (
-                            <Dialog>
-                                <DialogTrigger className="mt-5 block text-sm border border-[#383838] rounded-md px-2 py-1 cursor-pointer">
-                                    Settings
-                                </DialogTrigger>
-                                <DialogContent className="text-left bg-[#2c2c2c] border-none min-w-[60%] px-10 pb-10">
-                                    <DialogHeader className="text-left">
-                                        <DialogTitle className="flex items-center justify-between">
-                                            <div className="flex items-center">
-                                                {userData?.image && (
-                                                    <img
-                                                        src={userData.image}
-                                                        alt="profile"
-                                                        className="rounded-full w-[40px]"
-                                                    />
-                                                )}
-                                                <p className="font-bold text-[20px] ml-3">
-                                                    {userData?.username}
-                                                </p>
-                                            </div>
-                                            <SignOut />
-                                        </DialogTitle>
-                                        <DialogDescription className="mt-10">
-                                            <span className="block mt-5">
-                                                メールアドレス:
-                                                <span className="text-[#a8a8a8] font-bold">
-                                                    {userData?.email}
-                                                </span>
-                                            </span>
-                                            <span className="block mt-5">
-                                                Fruitsbaseを始めた日:
-                                                <span className="text-[#a8a8a8] font-bold">
-                                                    {formatDateToJST(
-                                                        userData?.createdAt,
+                        {userId === id &&
+                            userId !== null &&
+                            existingUserRecord && (
+                                <Dialog>
+                                    <DialogTrigger className="mt-5 block text-sm border border-[#383838] rounded-md px-2 py-1 cursor-pointer">
+                                        Settings
+                                    </DialogTrigger>
+                                    <DialogContent className="text-left bg-[#2c2c2c] border-none min-w-[60%] px-10 pb-10">
+                                        <DialogHeader className="text-left">
+                                            <DialogTitle className="flex items-center justify-between">
+                                                <div className="flex items-center">
+                                                    {existingUserRecord?.image && (
+                                                        <img
+                                                            src={
+                                                                existingUserRecord.image
+                                                            }
+                                                            alt="profile"
+                                                            className="rounded-full w-[40px]"
+                                                        />
                                                     )}
+                                                    <p className="font-bold text-[20px] ml-3">
+                                                        {
+                                                            existingUserRecord?.username
+                                                        }
+                                                    </p>
+                                                </div>
+                                                <SignOut />
+                                            </DialogTitle>
+                                            <DialogDescription className="mt-10">
+                                                <span className="block mt-5">
+                                                    メールアドレス:
+                                                    <span className="text-[#a8a8a8] font-bold">
+                                                        {
+                                                            existingUserRecord?.email
+                                                        }
+                                                    </span>
                                                 </span>
-                                            </span>
-                                        </DialogDescription>
-                                    </DialogHeader>
-                                    <UserDelete />
-                                </DialogContent>
-                            </Dialog>
-                        )}
+                                                <span className="block mt-5">
+                                                    Fruitsbaseを始めた日:
+                                                    <span className="text-[#a8a8a8] font-bold">
+                                                        {formatDateToJST(
+                                                            existingUserRecord?.createdAt,
+                                                        )}
+                                                    </span>
+                                                </span>
+                                            </DialogDescription>
+                                        </DialogHeader>
+                                        <UserDelete />
+                                    </DialogContent>
+                                </Dialog>
+                            )}
                     </div>
                     <div className="md:w-[70%] w-full">
                         <p className="text-gray-400 border-b border-blue-900 w-fit pb-1 md:mt-0 mt-10">
