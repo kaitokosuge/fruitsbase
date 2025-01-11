b89ef874d9ed3ba159740cc2e60877f800d03565
kaitokosuge <kaitokosuge.mi@gmail.com>
2025-01-11 19:50:47 +0900

    add: timelineの data cacheによる1分間キャッシュ処理

--- features/quizViews/QuizContainer.tsx
+++ features/quizViews/QuizContainer.tsx
@@ -1,28 +1,12 @@
-import prisma from '@/lib/prisma';
 import React from 'react';
 import QuizViews from './QuizViews';
+import { Quiz } from '@/models/Quiz';
 
 export default async function QuizContainer() {
-    const res = await fetch('https://www.fruitsbase.com/api/test', {
+    const res = await fetch('https://www.fruitsbase.com/api/quiz', {
         method: 'GET',
         next: { revalidate: 60 },
     });
-    const data = await res.json();
-    console.log('json placeholderから取得', data);
-
-    const quizzes = await prisma.quiz.findMany({
-        include: {
-            Category_Quiz: {
-                include: {
-                    category: true,
-                },
-            },
-            Option: true,
-            author: true,
-        },
-        orderBy: {
-            updatedAt: 'desc',
-        },
-    });
-    return <QuizViews quizzes={quizzes} />;
+    const data: { quizzes: Quiz[] } = await res.json();
+    return <QuizViews quizzes={data.quizzes} />;
 }
