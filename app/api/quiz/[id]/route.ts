831692173116e5a22d0e6658c32e551c59503631
kaitokosuge <kaitokosuge.mi@gmail.com>
2025-01-12 16:38:50 +0900

    fix: nextrequestへ変更

--- app/api/quiz/[id]/route.ts
+++ app/api/quiz/[id]/route.ts
@@ -1,8 +1,7 @@
 import prisma from '@/lib/prisma';
-import { NextApiRequest } from 'next';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET(req: NextApiRequest) {
+export async function GET(req: NextRequest) {
     const quizId = req.url?.split('/').pop();
     const quiz = await prisma.quiz.findFirst({
         where: {
