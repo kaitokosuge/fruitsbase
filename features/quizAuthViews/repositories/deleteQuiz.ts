bd37e2755821101a10455ebea9fc166f3ae27163
kaitokosuge <kaitokosuge.mi@gmail.com>
2025-01-13 19:41:49 +0900

    fix: クイズ削除後の遷移をトップに変更

--- features/quizAuthViews/repositories/deleteQuiz.ts
+++ features/quizAuthViews/repositories/deleteQuiz.ts
@@ -1,11 +1,11 @@
 import { redirect } from 'next/navigation';
 
-export const deleteQuiz = async (quizId: string, userId: string) => {
+export const deleteQuiz = async (quizId: string) => {
     const res = await fetch('/api/quiz/delete', {
         method: 'DELETE',
         body: JSON.stringify({ quizId: quizId }),
     });
     if (res.ok) {
-        return redirect(`/profile/${userId}`);
+        return redirect(`/`);
     }
 };
