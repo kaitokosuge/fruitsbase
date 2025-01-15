a554bf94fc71d02a84656b777ad5a541a3d037af
kaitokosuge <kaitokosuge.mi@gmail.com>
2025-01-15 20:26:17 +0900

    add: クイズ正解時にcorrect情報保存、クイズ回答時、attempt情報保存

---
+++ prisma/migrations/20250115110010_delete_data/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `User_Correct` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User_Correct" DROP COLUMN "createdAt";
