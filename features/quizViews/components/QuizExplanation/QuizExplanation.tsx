f2bf296492fc2d8ceb5a0fb297aae5eb7b7c9617
kaitokosuge <kaitokosuge.mi@gmail.com>
2025-01-17 19:28:28 +0900

    add: editorjsのオブジェクトをtype化

--- features/quizViews/components/QuizExplanation/QuizExplanation.tsx
+++ features/quizViews/components/QuizExplanation/QuizExplanation.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { atomOneDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { EditorObject } from '../../types/EditorObject';
 
 export default function QuizExplanation({
     explanation,
@@ -12,38 +13,22 @@ export default function QuizExplanation({
         <div>
             <p className="text-xs text-gray-300 mt-2">解説</p>
             <div className="mt-2">
-                {JSON.parse(explanation).map(
-                    (
-                        item:
-                            | {
-                                  id: string;
-                                  data: {
-                                      code: string;
-                                  };
-                              }
-                            | {
-                                  id: string;
-                                  data: {
-                                      text: string;
-                                  };
-                              },
-                    ) => (
-                        <div key={item.id} className="max-w-full">
-                            {'code' in item.data ? (
-                                <div className="text-[13px] mt-2 md:w-full w-[340px] mx-auto">
-                                    <SyntaxHighlighter
-                                        language="typescript"
-                                        style={atomOneDark}
-                                    >
-                                        {item.data.code}
-                                    </SyntaxHighlighter>
-                                </div>
-                            ) : (
-                                <div className="mt-2">{item.data.text}</div>
-                            )}
-                        </div>
-                    ),
-                )}
+                {JSON.parse(explanation).map((item: EditorObject) => (
+                    <div key={item.id} className="max-w-full">
+                        {'code' in item.data ? (
+                            <div className="text-[13px] mt-2 md:w-full w-[340px] mx-auto">
+                                <SyntaxHighlighter
+                                    language="typescript"
+                                    style={atomOneDark}
+                                >
+                                    {item.data.code}
+                                </SyntaxHighlighter>
+                            </div>
+                        ) : (
+                            <div className="mt-2">{item.data.text}</div>
+                        )}
+                    </div>
+                ))}
             </div>
         </div>
     );
