3ce03b2a455356e64ffef9b9f43cbdf480ea86df
kaitokosuge <kaitokosuge.mi@gmail.com>
2025-01-25 13:15:53 +0900

    add: タイムラインのクイズ正解時、回答ボタンの非活性化

--- features/timelineQuizView/components/AnswerBtn/AnswerBtn.tsx
+++ features/timelineQuizView/components/AnswerBtn/AnswerBtn.tsx
@@ -5,27 +5,40 @@ export default function AnswerBtn({
     quizId,
     loading,
     handleClickSubmit,
+    isTrue,
 }: {
     selectedOptionIds: string[];
     quizId: string;
     loading: boolean;
     handleClickSubmit: (selectedOptionIds: string[], quizId: string) => void;
+    isTrue: string;
 }) {
     return (
         <div>
-            <button
-                disabled={loading}
-                onClick={() => {
-                    if (selectedOptionIds.length === 0) {
-                        alert('選択肢は最低1つ選んでください');
-                        return;
-                    }
-                    handleClickSubmit(selectedOptionIds, quizId);
-                }}
-                className="ml-3 bg-[#333333] text-white px-5 py-1 block rounded-md font-bold opacity-40 hover:opacity-100 duration-200"
-            >
-                解答する
-            </button>
+            {Boolean(isTrue) ? (
+                <>
+                    <button
+                        disabled={false}
+                        className="ml-3 bg-[#333333] text-white px-5 py-1 block rounded-md font-bold opacity-40 duration-200"
+                    >
+                        解答する
+                    </button>
+                </>
+            ) : (
+                <button
+                    disabled={loading}
+                    onClick={() => {
+                        if (selectedOptionIds.length === 0) {
+                            alert('選択肢は最低1つ選んでください');
+                            return;
+                        }
+                        handleClickSubmit(selectedOptionIds, quizId);
+                    }}
+                    className="ml-3 bg-[#333333] text-white px-5 py-1 block rounded-md font-bold opacity-40 hover:opacity-100 duration-200"
+                >
+                    解答する
+                </button>
+            )}
         </div>
     );
 }
