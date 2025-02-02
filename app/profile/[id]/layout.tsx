7afd4374951ca78e8890755bdb4e7cacdb0408c4
kaitokosuge <kaitokosuge.mi@gmail.com>
2025-02-02 18:53:39 +0900

    add: メタ情報の取得

--- app/profile/[id]/layout.tsx
+++ app/profile/[id]/layout.tsx
@@ -1,6 +1,20 @@
 import Header from '@/components/Header/Header';
 import React from 'react';
 import { getUserData } from '../_features/repositories/getUserData/getUserData';
+import { Metadata } from 'next';
+
+export async function generateMetadata({
+    params,
+}: {
+    params: Promise<{ id: string }>;
+}): Promise<Metadata> {
+    const id = (await params).id;
+    const userData = await getUserData(id);
+
+    return {
+        title: `${userData?.username}のプロフィール`,
+    };
+}
 
 export default async function layout({
     params,
