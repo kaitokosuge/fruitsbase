1bf1cdacdadfb0ad6243d2977e8cd6cd15024edc
kaitokosuge <kaitokosuge.mi@gmail.com>
2025-02-06 22:09:52 +0900

    add: タイムラインのCLS改善

--- features/timelineQuizView/TimelineQuizzesViewContainer.tsx
+++ features/timelineQuizView/TimelineQuizzesViewContainer.tsx
@@ -3,6 +3,9 @@ import QuizViews from './QuizViews';
 import { PublicQuiz } from '@/models/PublicQuiz';
 
 export default async function TimelineQuizzesViewContainer() {
+    const delay = (ms: number) =>
+        new Promise((resolve) => setTimeout(resolve, ms));
+    await delay(2000);
     const res = await fetch(`${process.env.APP_URL}/api/quiz`, {
         method: 'GET',
         next: { revalidate: 60 },
